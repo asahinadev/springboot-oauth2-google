be047f0dd8e2f8f4ef0bfa3d01cc39734da6905c
asahinadev <dev.account.01@mail.mirageworld.jp>
2019-04-10 22:58:53 +0900

    名称変更

--- src/test/java/com/example/spring/google/ApplicationTests.java
+++ src/test/java/com/example/spring/google/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringOauth2GoogleApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
