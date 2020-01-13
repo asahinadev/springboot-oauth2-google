e9cf73f6041028e08e854435fce247e04374fa36
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-01-13 11:58:04 +0900

    セキュリティ初期設定　変更

--- src/main/java/com/example/spring/google/config/SecurityConfiguig.java
+++ src/main/java/com/example/spring/google/config/SecurityConfiguig.java
@@ -6,15 +6,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguig
-		extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguig extends WebSecurityConfigurerAdapter {
 
 	@Override
-	public void configure(WebSecurity web)
-			throws Exception {
+	public void configure(WebSecurity web) throws Exception {
 		super.configure(web);
 
 		web.ignoring().antMatchers(
@@ -29,8 +32,7 @@ public class SecurityConfiguig
 				// サウンドファイル
 				"/sound/**",
 				// WEB フォント
-				"/font/**",
-				"/fonts/**",
+				"/font/**", "/fonts/**",
 				// 外部ライブラリ
 				"/exlib/**"
 		/**/
@@ -38,42 +40,56 @@ public class SecurityConfiguig
 	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		super.configure(auth);
 	}
 
 	@Override
-	protected void configure(HttpSecurity http)
-			throws Exception {
-		super.configure(http);
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				// トップページは誰でもOK(自動遷移禁止）
+				.antMatchers("/").permitAll()
+				// エラーページは誰でもOK(自動遷移禁止）
+				.antMatchers("/error**").permitAll()
+				// ログイン必須
+				.anyRequest().authenticated();
 
-		http.formLogin().disable();
-		http.logout().disable();
+		this.configure(http.formLogin());
+		this.configure(http.httpBasic());
+		this.configure(http.logout());
+		this.configure(http.csrf());
+		this.configure(http.oauth2Login());
 
-		http.httpBasic().disable();
+	}
 
-		http.csrf().disable();
+	protected void configure(FormLoginConfigurer<HttpSecurity> form) {
+		form.disable();
+	}
 
-		http.oauth2Login()
+	protected void configure(LogoutConfigurer<HttpSecurity> logout) {
+	}
 
+	protected void configure(HttpBasicConfigurer<HttpSecurity> basic) {
+		basic.disable();
+	}
+
+	protected void configure(CsrfConfigurer<HttpSecurity> csrf) {
+		csrf.disable();
+	}
+
+	protected void configure(OAuth2LoginConfigurer<HttpSecurity> oauth) {
+		oauth
 				// 認証エンドポイント
-				.authorizationEndpoint()
-				.and()
+				.authorizationEndpoint().and()
 
 				// リダイレクトエンドポイント
-				.redirectionEndpoint()
-				.and()
+				.redirectionEndpoint().and()
 
 				// アクセストークンエンドポイント
-				.tokenEndpoint()
-				.and()
+				.tokenEndpoint().and()
 
 				// ユーザー情報エンドポイント
-				.userInfoEndpoint()
-				.and()
-
-		;
+				.userInfoEndpoint().and();
 
 	}
 
